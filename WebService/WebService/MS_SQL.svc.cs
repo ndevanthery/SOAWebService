83afa7e41bb8fff976a465d683d16017e8101394
ndevanthery <93075753+ndevanthery@users.noreply.github.com>
2022-04-08 11:06:52 +0200

    creation database, first implementation of webservice

---
+++ WebService/WebService/MS_SQL.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MS_SQL" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select MS_SQL.svc or MS_SQL.svc.cs at the Solution Explorer and start debugging.
+    public class MS_SQL : IMS_SQL
+    {
+        public float AddAmount(string UID, float quotas)
+        {
+            Console.WriteLine(UID + quotas);
+            return quotas;
+        }
+
+        public float transferMoney(string username, float quotas)
+        {
+            Console.WriteLine(username + quotas);
+            return quotas;
+        }
+    }
+}
